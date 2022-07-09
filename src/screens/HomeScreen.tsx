378c65539989bf66af00a27dd88dafa3149f0f5a
Ashish Sengar <65999078+Ashishss21@users.noreply.github.com>
2022-07-09 15:32:06 +0530

    Navbar and Main screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,24 +1,114 @@
 import React, { useState, useCallback } from 'react'
-import { Center, VStack, Text } from 'native-base';
-import ThemeToggle from '../components/ThemeToggle';
-import TaskItem from '../components/TaskItem';
+import { VStack, Text, Fab, Icon, useColorModeValue } from 'native-base';
+import { AntDesign } from '@expo/vector-icons'
+import shortid from 'shortid'
+import AnimatedColorBox from '../components/AnimatedColorBox'
+import HeroHeader from '../components/HeroHeader'
+import Navbar from '../components/Navbar'
+
+const initialData = [
+  {
+    id: shortid.generate(),
+    subject: 'Finish task 1',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Finish task 2',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Finish task 3',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Finish task 4',
+    done: false
+  }
+]
 
 export default function HomeScreen() {
-  const [checked, setChecked] = useState(false)
-  const handlePressCheckbox = useCallback(() => {
-    setChecked(prev => !prev)
+  const [data, setData] = useState(initialData)
+  const [editingItemId, setEditingItemId] = useState<string | null>(null)
+
+  const handleToggleTaskItem = useCallback(item => {
+    setData(prevData => {
+      const newData = [...prevData]
+      const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        done: !item.done
+      }
+      return newData
+    })
   }, [])
+  const handleChangeTaskItemSubject = useCallback((item, newSubject) => {
+    setData(prevData => {
+      const newData = [...prevData]
+      const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        subject: newSubject
+      }
+      return newData
+    })
+  }, [])
+  const handleFinishEditingTaskItem = useCallback(_item => {
+    setEditingItemId(null)
+  }, [])
+  const handlePressTaskItemLabel = useCallback(item => {
+    setEditingItemId(item.id)
+  }, [])
+  const handleRemoveItem = useCallback(item => {
+    setData(prevData => {
+      const newData = prevData.filter(i => i !== item)
+      return newData
+    })
+  }, [])
+
   return (
-    <Center
-      _dark={{ bg: 'blueGray.900' }}
-      _light={{ bg: 'blueGray.50' }}
-      px={4}
+    <AnimatedColorBox
       flex={1}
+      bg={useColorModeValue('warmGray.50', 'primary.900')}
+      w="full"
     >
-      <VStack space={5} alignItems="center">
-        <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} />
-        <ThemeToggle />
-      </VStack>
-    </Center>
+      <HeroHeader
+        title="Hey, What's Up!!"
+        image={require('../assets/main.png')}
+      >
+        <Navbar />
+      </HeroHeader>
+      <VStack
+        flex={1}
+        space={1}
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+        mt="-20px"
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        pt="20px"
+      ></VStack>
+      <Fab
+        position="absolute"
+        renderInPortal={false}
+        size="sm"
+        icon={<Icon color="white" as={<AntDesign name="plus" />} size="sm" />}
+        colorScheme={useColorModeValue('blue', 'darkBlue')}
+        bg={useColorModeValue('blue.500', 'blue.400')}
+        onPress={() => {
+          const id = shortid.generate()
+          setData([
+            {
+              id,
+              subject: '',
+              done: false
+            },
+            ...data
+          ])
+          setEditingItemId(id)
+        }}
+      />
+    </AnimatedColorBox>
   )
 }
