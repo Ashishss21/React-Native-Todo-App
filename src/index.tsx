f1bb76f924e3e42ce0da0427dc74002e24188e60
Ashish Sengar <65999078+Ashishss21@users.noreply.github.com>
2022-07-10 10:34:19 +0530

    Sidebar Implemented

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import HomeScreen from './screens/HomeScreen'
 import AboutScreen from './screens/AboutScreen'
+import Sidebar from './components/Sidebar'
 
 const Drawer = createDrawerNavigator()
 
 const App = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerContent={props => <Sidebar {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'back',
+        overlayColor: '#00000000'
+      }}
+    >
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="About" component={AboutScreen} />
     </Drawer.Navigator>
   )
 }
 
-export default App;
+export default App
